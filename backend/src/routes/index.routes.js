806eb1550bf8f08e28012be901a98d98ef3911f5
Yogaarsyad <08trioga08@gmail.com>
2025-05-10 16:15:15 +0700

    Frontend

--- backend/src/routes/index.routes.js
+++
@@ -1,9 +0,0 @@
-// src/routes/index.routes.js
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-  res.json({ message: 'API is working!' });
-});
-
-module.exports = router;
